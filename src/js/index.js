36bf58d97908a5247cef7f7617fe159e0697b0f9
marinosig <marinosignorini.it@gmail.com>
2022-04-27 19:01:50 +0000

    starting the project

--- src/js/index.js
+++ src/js/index.js
@@ -5,7 +5,7 @@ import ReactDOM from "react-dom";
 //include your index.scss file into the bundle
 import "../styles/index.css";
 
-//import your own components
+//import your own components to your project
 import Layout from "./layout";
 
 //render your react application
